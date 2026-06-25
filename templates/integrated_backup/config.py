b3335b38bf795ef9c1d27a1978e0338b15e3a1f0
AlexSM2025 <alexandra@rabonaventures.com>
2026-06-25 15:34:02 -0500

    Update config.py

--- templates/integrated_backup/config.py
+++ templates/integrated_backup/config.py
@@ -1,1 +1,33 @@
+FIELDS = {
 
+    "residence_line_1": {
+        "x": 1370,
+        "y": 30
+    },
+
+    "residence_line_2": {
+        "x": 1370,
+        "y": 55
+    },
+
+    "project_value": {
+        "x": 338,
+        "y": 340
+    },
+
+    "cash_discount": {
+        "x": 1444,
+        "y": 424
+    },
+
+    "hdm_carveout": {
+        "x": 1444,
+        "y": 517
+    },
+
+    "net_capital": {
+        "x": 810,
+        "y": 757
+    }
+
+}
